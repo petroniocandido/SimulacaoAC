38f711698403780e4cb2b23397d5dc8f080e112b
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2014-08-07 16:32:58 +0000

    Umidade e Temperatura médias

---
+++ src/br/edu/ifnmg/AutomatosCelulares/Simulacao/TemperaturaMedia.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.AutomatosCelulares.Simulacao;
+
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ *
+ * @author petronio
+ */
+public class TemperaturaMedia implements DistribuicaoSazonal<Double> {
+
+    @Override
+    public void setParametros(HashMap par) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Double processar(Date d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
